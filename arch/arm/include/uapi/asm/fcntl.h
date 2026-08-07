c47f7d00de9215256cde8931fafc86de84711459
Ma3a_XaTa <v.radulevich@gmail.com>
2026-08-07 07:16:41 +0300

    uapi: add O_TMPFILE compatibility for Pie

--- arch/arm/include/uapi/asm/fcntl.h
+++ arch/arm/include/uapi/asm/fcntl.h
@@ -6,6 +6,14 @@
 #define O_DIRECT	0200000	/* direct disk access hint - currently ignored */
 #define O_LARGEFILE	0400000
 
+#ifndef __O_TMPFILE
+#define __O_TMPFILE    020000000
+#endif
+
+#ifndef O_TMPFILE
+#define O_TMPFILE      (__O_TMPFILE | O_DIRECTORY)
+#endif
+
 #include <asm-generic/fcntl.h>
 
 #endif
